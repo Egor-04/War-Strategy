91265a501b7c9097347c0628f6ddd25656f6847b
Egor-04 <egor04m@gmail.com>
2021-06-30 22:16:53 +0300

    TouchPad

--- War Strategy/Assets/Scripts/Camera/TouchPad.cs
+++ War Strategy/Assets/Scripts/Camera/TouchPad.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TouchPad : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
@@ -11,10 +12,15 @@ public class TouchPad : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
     [Header("Values")]
     [SerializeField] private float _sensitivity = 5f;
 
+    [SerializeField] private Text _textX;
+    [SerializeField] private Text _textY;
+
+    private Image _touchPad;
     private bool _isDragged;
 
     private void Start()
     {
+        _touchPad = GetComponent<Image>();
         _camera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
@@ -30,9 +36,13 @@ public class TouchPad : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
         if (_isDragged)
         {
             Transform cameraTransform = _camera.transform;
-            Vector3 touchPosition = Input.GetTouch(0).position; 
+            Vector3 touchPosition = Input.mousePosition;
+            Vector3 touchDelta = new Vector3(touchPosition.x / _touchPad.rectTransform.sizeDelta.x / -2f, 0f, touchPosition.y / _touchPad.rectTransform.sizeDelta.y / -2f);
+            //Vector3 touchPosition = Input.GetTouch(0).position;
 
-            cameraTransform.position += new Vector3(touchPosition.x * _sensitivity * Time.deltaTime, 0f, touchPosition.z * _sensitivity * Time.deltaTime);
+            cameraTransform.position += new Vector3(touchDelta.x * _sensitivity, 0f, touchDelta.z * _sensitivity);
+            _textX.text = string.Format("{0:X:0}", touchDelta.x);
+            _textY.text = string.Format("{0:Y:0}", touchDelta.z);
             Debug.Log("Dragged");
         }
     }
